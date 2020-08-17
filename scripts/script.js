a624693e77d018f4b9f63116a4abb9d797fea0fa
AdnanGondal <adnangondal1@hotmail.com>
2020-08-17 15:40:37 +0100

    User is able to add to the display and also change the gameboard... Working to game mechanics

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,102 @@
 
 //    Players: 
-
-const Player = (name,marker) => {
-    const getName = () => name;
-    const getMarker = ()=> marker;
-
-    const makeMove = (marker)=>{
+const Player = (name,marker,turn) => {
     
-    return{getName,getMarker,makeMove}
-    };
+    const getName = () => name;
+    //const setName = (name)=> this.name = name;
+    
+    const getMarker = ()=> marker;
+    const getTurn = ()=>turn;
+
+    const toggleTurn = ()=>{
+        if (turn) turn=false;
+        else if (!turn) turn = true;
+    }
+
+    
+    return{getName,getMarker,toggleTurn,getTurn}
 };
 
 
-//   Control the Game Flow:
+
+//   Control the Game Flow: Most of the logic will be applied here: 
 
 const gameBoard = (()=>{
     
-    let _gomeBoard = [[0 ,0 ,0],
-                    [0, 0 ,0],
-                    [0,0,0]
+    let _gameBoard = [[0 ,0 ,0],
+                     [0, 0 ,0],
+                     [0,0,0]
                     //Make 0 for empty, X for player 1 and O for player 2
-                ];
-
-    let getBoard = ()=> _gomeBoard;
+                    // Access: [row][col]
+                    ];
+                    
+    let _Player1;
+    let _Player2;
+    let _isSet = false;
     
-    let updateBoard = (x,y,z)=>{
 
+    const setPlayers = (name1,name2)=>{
+        _Player1 = Player(name1,'X',true);
+        _Player2 = Player(name2,'O',false);
+        _isSet = true;
+    }
+
+    const isSet= () => _isSet;
+
+    const returnPlayer = ()=>{
+        if (_Player2.getTurn()) return _Player2;
+        return _Player1;
+    }
+    
+    const changeTurns = ()=>{
+        _Player1.toggleTurn();
+        _Player2.toggleTurn();
+    }
+
+    const getBoard = ()=> _gameBoard;
+    
+    const updateBoard = (index,mark)=>{
+        if (index==0) _gameBoard[0][0] = mark;
+        if (index==1) _gameBoard[0][1] = mark;  
+        if (index==2) _gameBoard[0][2] = mark;
+        if (index==3) _gameBoard[1][0] = mark;
+        if (index==4) _gameBoard[1][1] = mark;  
+        if (index==5) _gameBoard[1][2] = mark;
+        if (index==6) _gameBoard[2][0] = mark;
+        if (index==7) _gameBoard[2][1] = mark;  
+        if (index==8) _gameBoard[2][2] = mark;
     };
+
+    const checkForEnd = (mark)=>{
+        let fillCount = 0
+        for (let i=0;i<3;i++){
+            if (mark== _gameBoard[i][0] && _gameBoard[i][0] == _gameBoard[i][1] && _gameBoard[i][2]== _gameBoard[i][1]){
+                alert("rows win ...");
+            } else if(mark == _gameBoard[0][i] && _gameBoard[0][i] == _gameBoard[1][i] && _gameBoard[2][i]== _gameBoard[1][i]){
+                alert("col win");
+            }
+        }
+        if (mark == _gameBoard[0][0] && _gameBoard[0][0]==_gameBoard[1][1] && _gameBoard[2][2] == _gameBoard[1][1]) {
+            alert("diag win");
+        } else if (mark == _gameBoard[0][2] && _gameBoard[0][2]==_gameBoard[1][1] && _gameBoard[2][0] == _gameBoard[1][1]){
+            alert("other diag win");
+        }
+
+        for (let i=0;i<3;i++){
+            for (let j=0;j<3;j++){
+                if (_gameBoard[i[j] != 0]){fillCount++}
+            }
+        }
+
+        if (fillCount == 9) alert("Draw!");
+
+        
+    }
+
     
 
     return{
-        getBoard: getBoard, updateBoard: updateBoard,
+        getBoard, updateBoard, setPlayers,returnPlayer,changeTurns, isSet, checkForEnd
     };
 })();
 
@@ -38,4 +104,57 @@ const gameBoard = (()=>{
 // DOM Update:
  const displayController = (()=> {
     
+
+    let player1Name = document.querySelector("#player1-name");
+    let player2Name = document.querySelector("#player2-name");
+    let submitBut = document.querySelector('#name-submit-but')
+    let userMessage = document.querySelector('#game-message')
+    
+    let _entries = document.querySelectorAll(".game-entry");
+    
+    let render = ()=>{
+
+        submitBut.addEventListener('click',()=>{
+            if (!gameBoard.isSet()){
+            gameBoard.setPlayers(player1Name.value,player2Name.value);
+            userMessage.classList.remove("warning");
+            userMessage.classList.add('message');
+            userMessage.textContent = (`Welcome to the game! ${gameBoard.returnPlayer().getName()} will go first. `)
+            submitBut.disabled = true;
+            }
+        });
+        
+        //Update the board on click:
+        _entries.forEach((entry,i)=>{
+            entry.addEventListener('click',()=>{
+                
+                if (gameBoard.isSet()){
+                    let _player = gameBoard.returnPlayer();
+                    if (entry.textContent == " " ) {
+                        entry.textContent = _player.getMarker();
+                        gameBoard.updateBoard(i,entry.textContent)
+                        gameBoard.changeTurns();
+                        gameBoard.checkForEnd(entry.textContent);
+
+                        userMessage.classList.remove("warning");
+                        userMessage.classList.add('message');
+                        userMessage.textContent = `It is now ${gameBoard.returnPlayer().getName()}'s turn.`
+                    }
+                } else {
+                    userMessage.classList.add("warning");
+                    userMessage.textContent = ("Please sumbit player names!")
+                }
+            });
+
+        });
+    }
+
+    return {
+        render
+    }
+
+
  })();
+
+
+displayController.render();
